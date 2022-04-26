0c238bdbb4a97adc6abb967f2c06cd741227daee
Evan Shaw <e85.shaw@gmail.com>
2022-04-26 10:05:01 -0700

    update JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log('linked')
+
+//April 26th: 
+
+let calculator = {
+    read() {
+        this.a += prompt('a?')
+        this.b += prompt('b?')
+    }, 
+    sum() {
+        return this.a + this.b
+    }, 
+    mul() {
+        return this.a * this.b
+    }
+}
+ console.log(calculator.read())
