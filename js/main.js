8f8f91157fc007908456f3db98de8ecfc5a02fc3
Evan Shaw <e85.shaw@gmail.com>
2022-04-26 10:23:01 -0700

    update JavaScript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,64 @@
 console.log('linked')
 
-//April 26th: 
+//-------------------------------------April 26th: 
 
-let calculator = {
-    read() {
-        this.a += prompt('a?')
-        this.b += prompt('b?')
-    }, 
-    sum() {
-        return this.a + this.b
-    }, 
-    mul() {
-        return this.a * this.b
+// let calculator = {
+//     read() {
+//         this.a += prompt('a?')
+//         this.b += prompt('b?')
+//     }, 
+//     sum() {
+//         return this.a + this.b
+//     }, 
+//     mul() {
+//         return this.a * this.b
+//     }
+// }
+//  console.log(calculator.read())
+
+//  let user = {
+//      name: 'John', 
+//      age: 30,
+//  }
+
+//  let friend = {
+//      name: 'Jane',
+//      age: 31,
+//  }
+
+//  let clone = {}
+
+//  for (let key in user) {
+//      clone[key] = user[key]
+//  }
+// clone.name = 'Pete'
+//  console.log(clone)
+//  console.log(user)
+
+//  let salaries = { John: 100, Ann: 160, Pete: 130 }
+
+//  let sum = 0
+
+//  for (let key in salaries) {
+//      sum+=salaries[key]
+    
+//  }
+//  console.log(sum)
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+console.log(menu)
+
+function mulNum(obj) {
+    for (let key in obj) {
+      if(typeof obj[key] == 'number') {
+          obj[key] *= 2
+      }
     }
 }
- console.log(calculator.read())
+mulNum(menu)
+console.log(menu)
