4e53a38fe5faa47723c1dcd944d3044e5d8996dc
Evan Shaw <e85.shaw@gmail.com>
2022-04-26 14:08:42 -0700

    updated rest of JS, CSS, and HTML for the Day -300cards

--- js/main.js
+++ js/main.js
@@ -161,6 +161,133 @@ console.log('linked')
 
 // console.log(fruits.indexOf('pears'))
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`for loop number: ${i}`)
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`for loop number: ${i}`)
+// }
+
+// let animals = ['dog', 'goat', 'cat', 'boar']
+
+// for (let animal of animals) {
+//     console.log(animal)
+// }
+
+// let pen = {}
+
+// pen.color = 'blue'
+// pen.brand = 'bic'
+// pen.type = 'G2'
+
+// console.log(pen)
+
+// for (penEl in pen) {
+//     console.log(pen[penEl])
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${i} * ${i} = ${i*i}`)
+// }
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(`${i} * ${i} = ${i*i}`)
+//     i++
+// }
+
+// let count = 0
+
+// let countUp = () => {
+//     count += 1
+//     console.log(count)
+// }
+
+// countUp()
+// countUp()
+// countUp()
+// console.log(count)
+
+
+// const img = document.querySelector('img')
+
+
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data.message)
+//         img.src = (data.message)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+// let li = document.createElement('li')
+// li.textContent = 'blah blah'
+// let ul = document.querySelector('ul')
+
+// ul.appendChild(li)
+
+// constuctor: 
+
+// function MakeMonster(size, limbs, tail, teeth) {
+//     this.size = size
+//     this.limbs = limbs
+//     this.tail = tail
+//     this.teeth = teeth
+//     this.roar = function() {
+//         console.log('RAAWR')
+//     }
+//     this.specialATK = function() {
+//         console.log('Energy Blast')
+//     }
+// }
+
+// //class version: 
+
+// class MakeMonster2 {
+//     constructor(size, limbs, tail, teeth) {
+//         this.size = size
+//         this.limbs = limbs
+//         this.tail = tail
+//         this.teeth = teeth
+//     }
+//     roar() {
+//         console.log('RAAWR!')
+//     }
+//     specialATK() {
+//         console.log('Pew Pew Pew!')
+//     }
+// }
+
+
+// const godzilla = new MakeMonster('Xtra-Large', 4, true, 'Too many to count')
+// const kingKong = new MakeMonster2('Xtra-Large', 4, false, 'About 32')
+
+// console.log(godzilla.roar())
+// console.log(kingKong.specialATK())
+
+// const li = document.createElement('li')
+// li.textContent = 'hi'
+// document.querySelector('ul').appendChild(li)
+
+// let str = 'the panda was known for being absolutely adorable'
+
+// console.log(str.replaceAll('adorable', 'Awesome!'))
+
+// localStorage.setItem('bestFriend', 'SpongeBobSquarePants')
+// let friend = localStorage.getItem('bestFriend', 'SpongeBobSquarePants')
+// console.log(friend)
+
+// let count = 0
+
+// localStorage.getItem('count', count) 
+
+// document.querySelector('.block').addEventListener('click', event => {
+    
+//     if (count !== 0) {
+//         localStorage.setItem('count', count)
+//     }
+//     count +=1
+//     localStorage.setItem('count', count)
+//     console.log(event)
+// })
+
+
