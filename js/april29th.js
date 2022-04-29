1585044bf66189b7d0e336ee98d462ba844e5fc3
Evan Shaw <e85.shaw@gmail.com>
2022-04-29 10:39:28 -0700

    updated JS file

---
+++ js/april29th.js
@@ -0,0 +1,32 @@
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     }
+//     return confirm ('Did parents allow this?');
+    //age > 18 ? true : alert('Did parens allow this?')
+    // age > 18 || alert('did parents allow this')
+// }
+
+// function randomName(here) {
+//     if (here === undefined) {
+//         console.log('empty')
+//     }
+// }
+
+// let arr = [1, 15, 2]
+
+// arr.sort((a, b) => a - b)
+
+// console.log(arr)
+
+// arr.find( item => item === 2)
+
+// const todoText = todos.map(function(todo) {
+//     return todo.text;
+//  });
+
+//  const fruits = ['apples', ' oranges', 'pears', 'watermelon', 'papaya']
+
+//  console.log(fruits.indexOf('apples'))
+//  console.log(fruits[0])
+ 
