5ced94b780746777f2122e7ad3e946b6363ddc27
Evan Shaw <e85.shaw@gmail.com>
2022-05-03 08:20:10 -0700

    final anki cards for may3rd

--- js/may3rd.js
+++ js/may3rd.js
@@ -1,1 +1,41 @@
-console.log('hi')
+// console.log('May 3rd')
+
+// const colors = ['red', 'blue', 'green', 'yellow']
+
+// for (let col of colors) {
+//     console.log(col)
+// }
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+
+// const doubleUp = (obj) => {
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'number') {
+//             obj[key] *= 2
+//         }
+//     }
+// }
+
+// doubleUp(menu)
+// console.log(menu)
+
+// let user = {
+//     name: "John",
+//      age: 30,
+//      isAdmin: true
+// };
+
+// for(let obj in user) {
+//     console.log(user[obj])
+// }
+
+// let salaries = { John: 100, Ann: 160, Pete: 130 }
+// let sum = 0
+// for (let key in salaries) {
+   
+//     console.log(sum += salaries[key])
+// }
