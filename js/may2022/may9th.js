2d95d92077757d614b9e306746df19fb9b2eb29c
Evan Shaw <e85.shaw@gmail.com>
2022-05-09 09:44:12 -0700

    JS file update

--- js/may2022/may9th.js
+++ js/may2022/may9th.js
@@ -1,1 +1,69 @@
 console.log('May 9th')
+
+// const anObject = {
+//     myProp: 2
+//   };
+
+//   const anotherObject = Object.create(anObject)
+//   const yetAnotherObject = Object.create(anotherObject)
+//   const oneyetAnotherObject = Object.create(yetAnotherObject)
+ 
+
+//   console.log(anotherObject)
+//   console.log(yetAnotherObject)
+//   console.log(oneyetAnotherObject)
+
+// const random = function(){
+//     console.log('so random')
+// }
+
+// const pen = {
+//     brand: 'Bic', 
+//     style: 'Ballpoint', 
+//     color: 'Blue'
+// }
+
+// console.log(pen)
+
+
+// const myTeam = ['Charizard', 'Gengar', 'Dragapult', 'Greninja', 'Shedninja', 'Scizzor']
+// console.log(Array.isArray(myTeam))
+// // // const alphaba = myTeam.sort((a, b) => a - b)
+// // // console.log(alphaba)
+// // const onTeam = myTeam.includes('Gengar')
+// // console.log(onTeam)
+
+// let user = {
+//     name: "John",
+//      age: 30,
+//      isAdmin: true
+// };
+
+// for(prop in user) {
+//     console.log(`The ${prop} property of this object is ${user[prop]}`)
+// }
+
+// let salaries = { John: 100, Ann: 160, Pete: 130 }
+
+// sum = 0
+
+// for (let budget in salaries) {
+//     sum += salaries[budget]
+// }
+
+// console.log(sum)
+
+// const field {
+    
+// }
+
+
+// const myArray = [6, 19, 20];
+// const yourArray = [19, 81, 2];
+// for (let i = 0; i < myArray.length; i++) {
+//   for (let j = 0; j < yourArray.length; j++) {
+//     if (myArray[i] === yourArray[j]) {
+//       console.log('Both loops have the number: ' + yourArray[j])
+//     }
+//   }
+// };
