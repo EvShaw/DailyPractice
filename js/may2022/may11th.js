73e2d9be81783e24aee67404fcd1a768fcbeb7bf
Evan Shaw <e85.shaw@gmail.com>
2022-05-11 11:58:38 -0700

    update js file for daily anki deck

--- js/may2022/may11th.js
+++ js/may2022/may11th.js
@@ -1,1 +1,66 @@
 console.log('May 11th')
+
+
+// const user = {
+//     name: "jon",
+//     age: 32,
+// }
+
+// let clone = {}
+
+// for (let prop in user) {
+//     clone[prop] = [user[prop]]
+// }
+
+
+// console.log(clone.name)
+
+// clone.name = 'pete'
+
+// console.log('name' in clone)
+
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+// // if the loop has started, there is a property return false;
+//     }
+//     return true;
+// }
+
+// let schedule = {};
+// alert( isEmpty(schedule) ); // true
+//  schedule["8:30"] = "get up";
+// alert( isEmpty(schedule) ); // false
+
+// function EvenOdd(num){
+//     const ask = +prompt('Pick a Number')
+
+//     for (let i = 0; i < ask; i++) {
+//         if(i%2 === 0){
+//             console.log(`${i} is even`)
+//         } else {
+//             console.log(`${i} is odd`)
+//         }
+//     }
+
+// }
+
+
+// const evensOrOdds = n => n % 2 === 0 ? console.log('even') : console.log('odd')
+
+
+
+
+// evensOrOdds(123)
+
+// const user = users.find(item => item.id == 2)
+// let arr = [1, 22, 2, 15, 6];
+
+// console.log(arr.sort((a, b)=> a - b))
+
+// console.log(arr.concat([11, 23], 111))
+
+// [3,4] alert( arr.concat([3, 4]) ); 
+
+
+// [3,4] and [5,6] alert( arr.concat([3, 4], [5, 6]) );
