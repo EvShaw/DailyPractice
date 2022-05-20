c6e067a0ad73dd05a78ae90869ca352cdaa810ab
Evan Shaw <e85.shaw@gmail.com>
2022-05-19 21:18:10 -0700

    anki deck finished

--- js/may2022/may19th.js
+++ js/may2022/may19th.js
@@ -1,1 +1,51 @@
 console.log('May 19th, 2022')
+
+//fs.readFile(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+// if (err) throw err
+// console.log('file renamed...')
+// })
+
+// const squares = n => Math.sqrt(n) % 1 === 0
+
+// function houseOne() {
+//     setTimeout(() => {
+//         console.log('Paper delivered to house 1')
+//         setTimeout(() => {
+//             console.log('Paper delivered to house 2')
+//             setTimeout(() => {
+//                 console.log('Paper delivered to house 3')
+//             }, 3000)
+//         }, 4000)
+//     }, 5000)
+// }
+// houseOne()
+
+// class Animal {
+//     constructor(name, breed) {
+//         this._name = name
+//     }
+//     get name() {
+//         return  this._name
+//     }
+//     get breed() {
+//         return this._breed
+//     }
+//     speak() {
+//         console.log('bark')
+//     }
+// }
+
+// fetch(urlHere) {
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+// }
+
+// async function getACuteDogPhoto() {
+//     const res = await fetch('urlHere')
+//     const data =await res.json()
+//     console.log(data)
+// }
+
+// getACuteDogPhoto
