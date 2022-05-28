b1d5dde9fd3ffe4753c2988465724c8041e34691
Evan Shaw <e85.shaw@gmail.com>
2022-05-28 08:01:15 -0700

    Anki Deck for May 28th finished

--- js/may2022/may28th.js
+++ js/may2022/may28th.js
@@ -1,3 +1,63 @@
 console.log('May 28th, 2022')
 
-console.log('new push to git workflow')
+// console.log('new push to git workflow')
+
+
+// function sandwhich(bread, condiments, meat, toppings) {
+//     this.bread = bread
+//     this.condiements = condiments
+//     this.meat = meat
+//     this.toppings = toppings
+//     this.sliced = function () {
+//         console.log('sliced bread')
+//     }
+// }
+
+// const myLunch = new Sandwhich('wheat', 'mustard', 'turkey', ['lettuce', 'tomato', 'bacon', 'avacado'])
+
+
+// console.log(myLunch)
+
+// class BetterBiggerTastierSandwhich {
+//     constructor(bread, condiments, meat, toppings) {
+//         this._bread = bread
+//         this._condiements = condiments
+//         this._meat = meat
+//         this._toppings = toppings
+//     }
+//     get bread() {
+//         return   this._bread
+//     }
+//     get condiments() {
+//         return   this._condiements
+//     }
+//     get meat () {
+//         return   this._meat
+//     }
+//     get toppings () {
+//         return   this._toppings
+//     }
+//     sliced() {
+//         console.log('sliced bread')
+//     }
+// }
+
+// const myDinner = new BetterBiggerTastierSandwhich('wheat', 'mustard', 'turkey', ['lettuce', 'tomato', 'bacon', 'avacado'])
+
+// let user = {
+//     name: "John",
+//      age: 30,
+//      isAdmin: true
+// };
+
+// for (prop in user) {
+//     console.log(prop)
+//     console.log(user[prop])
+// }
+
+// let num = 0
+
+// while (num < 12) {
+//     console.log(num)
+//     num+=2
+// }
