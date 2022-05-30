6461c2ac13e481c9c62e3fb1f9ec40c101671ce5
Evan Shaw <e85.shaw@gmail.com>
2022-05-29 20:52:19 -0700

    Anki deck finished

--- js/may2022/may29th.js
+++ js/may2022/may29th.js
@@ -1,1 +1,37 @@
 console.log('May 29th, 2022')
+
+// for (let i = '#'; i.length < 8; i+='#') {
+//     console.log(i)
+// }
+// console.log('########')
+
+// for (let i = '#######'; i.length > 0; i= i.slice(1)) {
+//     console.log(i)
+// }
+
+// async function getPhoto(){
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+// getPhoto()
+// async function getPhoto(){
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+// getPhoto()
+
+// data.subclasses.forEach(obj => {
+//     console.log(obj.name)
+
+//     const li = document.createElement('li')
+//     li.innerContent = "Hi!"
+//     document.querySelector('ul').appendChild(li)
+// })
+let num = 121
+if (num > 0 && num < 100) {
+    console.log('yes')
+} 
