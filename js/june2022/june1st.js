ab9a0b9ade65ae2e1796a2fc9bc3131fefdef24e
Evan Shaw <e85.shaw@gmail.com>
2022-06-01 21:33:52 -0700

    update to JS file

---
+++ js/june2022/june1st.js
@@ -0,0 +1,83 @@
+console.log('june 1st, 2022')
+
+
+// let username = ''
+// let defaultName
+
+
+// if (username) {
+//     defaultName = username
+// } else {
+//     defaultName = 'Stranger'
+// }
+
+// console.log(defaultName)
+
+// let username = ''
+// let defaultName = username || 'Stranger'
+
+// console.log(defaultName)
+
+// console.log('7'.padStart(3, '0'))
+
+// let board = ''
+// const size = 8
+
+// for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++){
+//         if((i + j) % 2 === 0) {
+//             board += ' '
+//         } else {
+//             board += '#'
+//         }
+//     }
+//     board += '\n'
+// }
+
+// console.log(board)
+
+// function PizzaMaker(size, sauce, toppings, crust) {
+//     this.size = size
+//     this.sauce = sauce
+//     this.toppings = toppings
+//     this.crust = crust
+//     this.deliver = function() {
+//         console.log('on its way')
+//     }
+// }
+
+// const myPizza = new PizzaMaker('medium', 'marinara', 'all', 'norm')
+
+// console.log(myPizza.deliver())
+
+// class pizzaMachine{
+//     constructor(size, sauce) {
+//         this._size = size
+//         this._sauce = sauce
+//     }
+//     get size(){
+//         return this._size
+//     }
+//     get sauce(){
+//         return this._sauce
+//     }
+//     deliver(){
+//         console.log('incoming!')
+//     }
+// }
+
+// const myPizza2 = new pizzaMachine('large', 'none')
+
+// console.log(myPizza2.sauce)
+
+// async function dogPhoto() {
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+// dogPhoto()
+
+async function functionNAme(){
+    //code
+}
