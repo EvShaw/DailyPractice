86695064cb8f8d4f62ca617cff371b26a245b2b7
Evan Shaw <e85.shaw@gmail.com>
2022-06-11 13:40:52 -0700

    daily anki deck completed

---
+++ js/june2022/june11th.js
@@ -0,0 +1,72 @@
+console.log('June 11th')
+
+// const todaysDate = new Date
+
+// console.log(todaysDate)
+
+// let username = ''
+// let defaultName = username || 'Stranger'
+
+// if(username) {
+//    defaultName = username
+// } else {
+//     defaultName = 'Stranger'
+// }
+
+// console.log(defaultName)
+// let num = 0
+// while(num < 12) {
+//     console.log(num+=2)
+
+// // }
+
+// let user = {'name':'Ben', 'age':'13'}
+
+// let newUser = Object.assign({}, user)
+
+// console.log(newUser)
+
+// for (let i = '#'; i.length < 8; i += '#') {
+//     console.log(i)
+// }
+
+// console.log('#'.repeat(8))
+
+// for (let i = '#######'; i.length > 0; i = i.slice(1)) {
+//     console.log(i)
+// }
+
+// npm install cors --save
+
+// const cors = require('cors')
+// app.use('cors')
+
+// function isEmpty(obj) {
+//     for(let key in obj) {
+//         return true
+//     } return false
+// }
+
+// const obj2 = {'name':'none'}
+
+// console.log(isEmpty(obj2))
+
+// app.get('/', (request, response) =>{
+//     //
+// })
+
+// const squares = n => Math.sqrt(n) % 1 === 0 ? 'yes' : 'no'
+
+// document.querySelector('idHere').addEventListener('mouseover', (e) =>{
+//     console.log(e)
+// })
+
+// async function fetchPhoto() {
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+// fetchPhoto()
+
+const MongoClient = require('mongodb').MongoClient
