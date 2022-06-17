2188ed5c66f78109452b64342ae486b1dbf68da8
Evan Shaw <e85.shaw@gmail.com>
2022-06-17 16:55:27 -0700

    Anki deck completed. 48 days straight, daily average: 132 cards

--- js/june2022/june17th.js
+++ js/june2022/june17th.js
@@ -24,3 +24,46 @@ console.log(today)
 // console.log(favoritePhrase)
 
 // console.log(String(6).padStart(10, '0'))
+
+// Array.find(item => item.id == 2)
+
+// Response.render('index.ejs'), {info : data}
+
+// arrayOfUser.find(person => person.id == 2)
+
+// const http = require('http')
+// const fs = require('fs')
+// const url = require('url')
+
+// const express = require('express')
+// const app = express()
+// app.listen(3000, _ => {
+//     console.log('on 3000')
+// })
+
+// app.post('/quotes', (req, res) => {
+//     console.log('hi')
+// })
+
+// npm install express --save
+
+// function rounded(num) {
+//     Math.round((num)*100)/100
+
+
+// }
+
+// console.log(rounded(23.12345651))
+
+// app.get('/api/person/:id', () => {
+//     const request = req.params.id
+// })
+
+// const choice = document.querySelector('input').value.replaceAll(' ','-')
+
+// npm init
+// const MongoClient = require('mongodb').MongoClient
+
+// const nameHere.input.replaceAll(' ', '-').replaceAll('.','').toLowerCase()
+
+console.log(Math.round((23.456789123)*100)/100)
