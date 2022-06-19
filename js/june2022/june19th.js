065ac2c3b13dfa66e63828f875c05b1a9a7eb964
Evan Shaw <e85.shaw@gmail.com>
2022-06-19 08:51:07 -0700

    Anki deck finished. 50 day streak, 131 card average.

--- js/june2022/june19th.js
+++ js/june2022/june19th.js
@@ -1,2 +1,35 @@
 const today = new Date
 console.log(today)
+
+
+// const string = 'testing'
+// const getMiddle = s => s.slice((s.length-1)/2, s.length/2+1)
+
+// console.log(getMiddle(string))
+
+// const num = 11
+
+// console.log(Number.isInteger(num))
+// let objectA = {a: 1, b: 2}
+// Object.assign(objectA, {b: 3, c: 4})
+// console.log(objectA)
+
+// let objectB = Object.assign({}, [objectA, {b: 3, c: 4}])
+
+// console.log(objectB)
+//for (let myElement of myArray) {
+//code here
+// }
+
+// console.log(Math.pow(2, 10))
+
+// const allEqual = arr => arr.every(val => val === arr[0])
+// // const allEqual = arr => arr.every(val => val === arr[0])
+
+
+// console.log(allEqual([22,22,22]))
+
+// npm install nodemon --save-dev
+// app.get('/', (request, response) => {
+//     response.sendFile(__dirname + '/index.html')
+// })
