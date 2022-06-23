1929e2f1e52a2039cae4b3169433c0506a3e8d7c
Evan Shaw <e85.shaw@gmail.com>
2022-06-22 22:22:08 -0700

    Anki deck completed: 72 cards, 53 days straight, 130 daily average

--- js/june2022/june22nd.js
+++ js/june2022/june22nd.js
@@ -1,2 +1,61 @@
 const today = new Date
 console.log(today)
+
+
+// let num = 1
+// for (let i = 0; i < 10; i++) {
+//     console.log(num*=2)
+// }
+
+// let board = ''
+// let size = 8
+
+// for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++) {
+//         if ((i + j) % 2 === 0) {
+//             board += ' '
+//         } else {
+//             board += '#'
+//         }
+
+//     }
+//     board += '\n'
+// }
+
+// console.log(board)
+
+// npm install nodemon --save-dev
+// npm run dev
+//npm install body-parser --save
+// cosnt MongoClient = require('mongodb').MongoClient
+// path.parse(__filename)
+// path.parse(__filename).base)
+
+// const getMid = s => s.slice((s.length-1)/2, s.length/2+1)
+// console.log(getMid('test'))
+// const cors = require('cors')
+// app.use(cors())
+// let salaries = { John: 100, Ann: 160, Pete: 130 }
+
+// let sum = 0
+// for(let key in salaries) {
+//     sum += salaries[key]
+// }
+// console.log(sum)
+// Array.from('bindingNameHere').forEach(ele => ele.addEventListener('click') (e) => {
+//     console.log(e)
+// })
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     hobbies: ['music', 'movies', 'sports'],
+//     address: {
+//         street: '40 main st',
+//         city: 'boston',
+//         state: 'MA',
+//     }
+// }
+// console.log(person.address.city)
+// const goMID = s => s.slice((s.length-1)/2, s.length/2+1)
+// console.log(goMID('Jumbo'))
