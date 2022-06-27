ae6998bd1fb1502308e596ad76a8b7c3dabb71f9
Evan Shaw <e85.shaw@gmail.com>
2022-06-26 17:03:05 -0700

    Daily Anki deck finished. 58 cards, 128 card daily average, 57 days streak

--- js/june2022/june26th.js
+++ js/june2022/june26th.js
@@ -1,3 +1,48 @@
 const today = new Date
 console.log(today)
 
+// const colors = ['red', 'black', 'green', 'yellow', 'blue', 'pink']
+
+// function whatColorsIsPowerRangers(arr) {
+//     arr.forEach(color => {
+//       console.log(color)
+//     })
+
+// }
+
+// whatColorsIsPowerRangers(colors)
+
+// async function functionName() {
+//   const res = await fetch(urlHere)
+//   const data = res.json()
+//   console.log(data)
+// }
+
+// let aString = 'the panda fell over'
+
+// console.log(aString.indexOf('d'))
+// console.log(aString[7].charCodeAt())
+
+// console.log(os.uptime())
+
+// const generateAnID = () => {
+//     const maxID = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
+//     return maxID + 1
+// }
+// app.get('/api/persons/:id', () => {
+//     const request = req.params.id
+// })
+
+// npm install nodemon --save-dev
+
+// inputVarHere.replaceAll(' ', '-').replaceAll('.', '').toLowerCase()
+
+// function removeAdjDupes(arr) {
+//     return [...arr].filter((a, i) => a !== arr[i - 1])
+// }
+
+// console.log(removeAdjDupes('AAAABBBCCDAABBB'))
+
+// function findMultiples(int,limit){
+//     return Array(Math.floor(limit/int)).fill(1).map((x,i)=>int*(i+1));
+//   }
