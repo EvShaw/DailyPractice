4185f4dc38b178f1ad77eb02b4ecfbbbbfd1dc5a
Evan Shaw <e85.shaw@gmail.com>
2022-06-30 20:04:02 -0700

    Anki deck finished. 64 cards, 127 daily card average. 61 day streak

--- js/june2022/june30th.js
+++ js/june2022/june30th.js
@@ -1,5 +1,58 @@
 const today = new Date
 console.log(today)
 
+// let tester = ''
+
+// if(tester){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// console.log(tester)
+// console.log(Number.isInteger(23))
+// install npmmongodb --save
 
 
+// const persons = ['fred', 'barney', 'wilma', 'betty']
+
+// const generateID = () => {
+//     const maxID = persons.length > 0
+//     ? Math.max(...persons.map(n => n.id))
+//     : 0
+//     return maxID +1
+// }
+
+// app.get('/', (request, responst') => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+// const choice = document.querySelector('input').value.replaceAll(' ', '-') 
+
+// for(let i = 0; i < persons.length; i++) {
+// persons[i] = persons[i][0].toUpperCase() + persons[i].slice(1)
+// }
+
+// console.log(persons)
+
+// app.get('/', (request, response) => {
+//     response.sendFile(__dirname + '/index.html')
+// })
+// const persons = ['fred', 'barney', 'wilma', 'betty']
+// for(let i = 0; i < persons.length; i++) {
+//     persons[i] = persons[i][0].toUpperCase() + persons[i].slice(1)
+// }
+// console.log(persons)
+
+// const getMiddle = str => str.slice((str.length-1)/2, s.length/2+1)
+
+// const allEquals = arr => arr.every(val => val === arr[0])
+
+// console.log(allEquals([2, 2, 2]))
+
+// ObjectNAmeHere.prototype.PropertyNameHere = valueHere
+
+// const MongoClient = require('mongodb').MongoClient
+
+// MongoClient.connect('mongodb-connection-string', (err, client) => {
+   
+// })
