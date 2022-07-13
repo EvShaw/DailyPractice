07432bfdc5c11fa822478ff00bcf837c336bdba8
Evan Shaw <e85.shaw@gmail.com>
2022-07-13 05:26:44 -0700

    Anki deck file setup

---
+++ js/july2022/july13th.js
@@ -0,0 +1,18 @@
+const today = new Date
+console.log(today)
+
+// const isPailin = str => str.split('').reverse().join('') === str ? 'yes' : 'no'
+
+// console.log(isPailin('racecar'))
+
+// console.log(String('7').padStart(3, 0))
+
+// app.get('/api/persons/:id', () => {
+//     const request = req.params.id
+// })
+
+// npm install mongodb --save
+// const MongoClient = require('mongodb').MongoClient
+
+// myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`))
+
