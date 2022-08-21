f50b94597ea31b2c241c8acb047c92abad34b423
Evan Shaw <e85.shaw@gmail.com>
2022-08-21 16:05:47 -0700

    Anki deck completed! 105 cards, 112 card average, 113 day streak!

---
+++ js/aug2022/aug21st.js
@@ -0,0 +1,56 @@
+const today = new Date
+console.log(today)
+
+// const thisArr = ['bread', 'cheese', 'lunch meat', 'something']
+
+// for(let i = 0; i < thisArr.length; i++) {
+
+//     thisArr[i] = thisArr[i][0].toUpperCase() + thisArr[i].slice(1)
+// }
+
+// console.log(thisArr)
+
+// document.querySelector('idNameHere').ariaValueMax.replaceAll(' ', '-').replaceAll('.', '').toLowerCase()
+
+
+// function countSheeps(arrayOfSheep) {
+//     let sheepCount = 0
+
+//     arrayOfSheep.forEach(sheep => {
+//         if (sheep == true) sheepCount++
+//     })
+
+//     return sheepCount
+// }
+
+// const countSheeps = arr => arr.filter(Boolean).length
+
+// const swapOut = arr => {
+//     let pairs = {'e':'s', 'n':'w'}
+
+//     return arr.split('').map( v => pairs[v]).join('')
+// }
+// console.log(swapOut('en'))
+
+// const a = [...Array(4)]
+// const b = a.map((_, i) => i)
+// const c = [...Array(4)].map((_, i) => i)
+// const d = Array.prototype.map.call(a, (_, i) => i)
+// console.log(a)   //output: [ undefined, undefined, undefined, undefined ]
+// console.log(b)   //output: [ 0, 1, 2, 3 ]
+// console.log(c)   //output: [ 0, 1, 2, 3 ]
+// console.log(d)   //output: [ 0, 1, 2, 3 ]
+
+// const a = [2,4,6,8]
+// const newA = a.map((el, ind, arr) => arr[arr.length - ind - 1])
+// console.log(newA)
+
+// function isEmpty(obj) {
+//     if(obj) return false
+//     else return true
+// }
+
+// console.log(isEmpty())
+
+
+
